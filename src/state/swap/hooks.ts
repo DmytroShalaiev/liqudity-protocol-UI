95bac4558a4adaf5461838319969d30967accc0f
dmytro.shalaiev <dmytro.shalaiev@jcash.co>
2021-06-17 11:41:04 +0300

    срфтпуы

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -89,9 +89,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', // v2 factory
+  '0xE91Df22C105A0124AEd0D8598a68377a7df8Da9c', // factory 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // v2 router 02
+  '0x0535B691f48e0cf25e1F180ab7c6fCF75fF99f3E' // router 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
 ]
 
 /**
